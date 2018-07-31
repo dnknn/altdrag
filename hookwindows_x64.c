a91ffc534167c1a0276fa40bbcc7dd866bdbe1c1
strayge <strayge@gmail.com>
2018-07-31 10:46:59 +0600

    fixed warnings

--- hookwindows_x64.c
+++ hookwindows_x64.c
@@ -34,6 +34,9 @@ HHOOK msghook = NULL;
 // Include stuff
 #include "include/error.c"
 
+// Fwd declarations
+int HookSystem();
+
 // Entry point
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow) {
   g_hinst = hInst;
